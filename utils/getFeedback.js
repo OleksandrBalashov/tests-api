b025224fac8d4f30c39a4c07c21ae4d7916c4abe
OleksandrBalashov <aleksandr.balashov.sax@gmal.com>
2021-07-12 23:02:55 +0300

    reverse fixed feedback

--- utils/getFeedback.js
+++ utils/getFeedback.js
@@ -1,16 +1,12 @@
 const feedback = (allTests, userTests) => {
   const result = allTests.filter(({ _id, rightAnswer }) => {
-    return userTests.filter(({ id, answer }) =>
-      String(_id) === id && rightAnswer === answer ? answer : null
-    );
+    const stringId = String(_id);
 
-    // const stringId = String(_id);
-
-    // for (let i = 0; i < userTests.length; i++) {
-    //   if (stringId === userTests[i].id && rightAnswer === userTests[i].answer) {
-    //     return userTests[i].answer;
-    //   }
-    // }
+    for (let i = 0; i < userTests.length; i++) {
+      if (stringId === userTests[i].id && rightAnswer === userTests[i].answer) {
+        return userTests[i].answer;
+      }
+    }
   });
   const feedback = `${Math.floor((result.length / 12) * 100)}%`;
   return feedback;
